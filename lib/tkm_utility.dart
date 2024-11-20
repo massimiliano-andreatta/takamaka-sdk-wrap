df985d866ab36ebc56e0725d1c401fc458f247cd
Andreatta Massimiliano <massimiliano.andreatta@gmail.com>
2024-11-20 09:00:19 +0100

    Fix function "TkmUtility.isValidBlockchainAddress"

--- lib/tkm_utility.dart
+++ lib/tkm_utility.dart
@@ -1,7 +1,6 @@
 import 'dart:convert';
 
 class TkmUtility {
-
   static bool isValidBlockchainAddress(String address) {
     // Check the length
     if (address.length != 44) {
@@ -11,9 +10,9 @@ class TkmUtility {
     // Check Base64URL encoding
     try {
       // Decode the string using Base64URL
-      final decoded = base64Url.decode(address);
+      final decoded = base64Url.decode(address.replaceAll(".", "="));
       // Re-encode the decoded data back to Base64URL
-      final reEncoded = base64Url.encode(decoded);
+      final reEncoded = base64Url.encode(decoded).replaceAll("=", ".");
 
       // Verify that the original string matches the re-encoded string
       if (address != reEncoded) {
@@ -25,5 +24,4 @@ class TkmUtility {
 
     return true;
   }
-
 }
